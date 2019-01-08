e2e9b623a3ca03fe9b910d3ab427db6998afe532
truyenho95 <truyenho.webdev@gmail.com>
2019-01-08 16:33:57 +0700

    Edit CustomerServiceImpl class

--- src/main/java/com/truyenho/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/truyenho/service/impl/CustomerServiceImpl.java
@@ -5,6 +5,8 @@ import com.truyenho.model.Province;
 import com.truyenho.repository.CustomerRepository;
 import com.truyenho.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public class CustomerServiceImpl implements CustomerService {
 
@@ -15,13 +17,13 @@ public class CustomerServiceImpl implements CustomerService {
   }
 
   @Override
-  public Iterable<Customer> findAll() {
-    return customerRepository.findAll();
+  public Iterable<Customer> findAllByProvince(Province province) {
+    return customerRepository.findAllByProvince(province);
   }
 
   @Override
-  public Iterable<Customer> findAllByProvince(Province province) {
-    return customerRepository.findAllByProvince(province);
+  public Page<Customer> findAll(Pageable pageable) {
+    return customerRepository.findAll(pageable);
   }
 
   @Override
@@ -37,5 +39,10 @@ public class CustomerServiceImpl implements CustomerService {
   @Override
   public void remove(Long id) {
     customerRepository.delete(id);
+  }
+
+  @Override
+  public Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable) {
+    return customerRepository.findAllByFirstNameContaining(firstname, pageable);
   }
 }
