43a6544e79fdb9e972452ad393759a3aa7769dce
truyenho95 <truyenho.webdev@gmail.com>
2019-01-08 16:35:00 +0700

    Add findAllByFirstNameContaining method and change findAll method in CustomerService interface

--- src/main/java/com/truyenho/service/CustomerService.java
+++ src/main/java/com/truyenho/service/CustomerService.java
@@ -2,15 +2,21 @@ package com.truyenho.service;
 
 import com.truyenho.model.Customer;
 import com.truyenho.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CustomerService {
-  Iterable<Customer> findAll();
+//  Iterable<Customer> findAll();
 
   Iterable<Customer> findAllByProvince(Province province);
+
+  Page<Customer> findAll(Pageable pageable);
 
   Customer findById(Long id);
 
   void save(Customer customer);
 
   void remove(Long id);
+
+  Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable);
 }
