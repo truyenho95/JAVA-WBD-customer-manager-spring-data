f1499ce80d2d1cb724372869a79bd684981d6339
truyenho95 <truyenho.webdev@gmail.com>
2019-01-08 16:33:01 +0700

    Change interface CustomerRepository

--- src/main/java/com/truyenho/repository/CustomerRepository.java
+++ src/main/java/com/truyenho/repository/CustomerRepository.java
@@ -2,8 +2,11 @@ package com.truyenho.repository;
 
 import com.truyenho.model.Customer;
 import com.truyenho.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
   Iterable<Customer> findAllByProvince(Province province);
+  Page<Customer> findAllByFirstNameContaining(String firstname, Pageable pageable);
 }
